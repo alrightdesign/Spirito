78e1b00a066e542f950214f0cd632b656027482f
Elena Capellini <elena.capellini@gmail.com>
2014-01-27 16:22:56 +0100

    Merge branch 'master' of https://github.com/alrightdesign/Spirito
    Conflicts:
    	css/lele.css

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
-$('#headerImage').css({'opacity':'0'});
+
+
+function articleHeaderFadeIn() {
+	$('#headerImage').css({'opacity':'0'});
+    //alert("pinogino");
+    $('#headerImage').animate({'opacity':'1'}, 1000);
+}
+
+
+function setHeaderParallax() {
+
+	$(window).scroll(function() {
+		
+		var vel = 6;
+		var e = $(window).scrollTop()/vel;
+		var	t = "50% " + ( 50 - e ) + "%";
+
+		$("#headerImage").css( {backgroundPosition:t} );
+	});
+}
+
+
 
 $(document).ready(function() {
-
-    //alert("pinogino");
-
-    $('#headerImage').animate({'opacity':'1'}, 1000);
+	
+	articleHeaderFadeIn();
+	setHeaderParallax();
 
 });
+
+
